90b0a532dc8a8ad03fb3211056da65cafa027126
VictorHOgawa <victor.ogawa@gmail.com>
2025-02-21 12:20:18 -0300

    clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Poppins } from "next/font/google";
 import { CookiesProvider } from "next-client-cookies/server";
 import "swiper/css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Foco Assessoria",
@@ -21,6 +22,13 @@ export default function RootLayout({
 }>) {
   return (
     <html>
+      <Script type="text/javascript" id="clarity">
+        {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "qdcx4ukh2o");`}
+      </Script>
       <CookiesProvider>
         <body className={`${poppins.className} `}>{children}</body>
       </CookiesProvider>
