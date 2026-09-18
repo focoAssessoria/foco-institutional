064dc8aef9f24e6b949b8f53b4854db6c7aa59de
Victor Ogawa <tecnologia.focosaudeanimal@gmail.com>
2026-09-18 15:58:52 -0300

    chore(other): revert

--- src/app/components/VirtualTour.tsx
+++ src/app/components/VirtualTour.tsx
@@ -1,9 +1,9 @@
 // components/VirtualTour.tsx
 export default function VirtualTour() {
   return (
-    <div className="h-full w-full bg-red-500 md:aspect-video">
+    <div className="h-full w-full md:aspect-video">
       <iframe
-        src="http://www.elo3d.com.br/FOCO_CONSUTORIA/FOCO_ATUAL/tour.html"
+        src="https://www.elo3d.com.br/FOCO_CONSUTORIA/FOCO_ATUAL/tour.html"
         title="Virtual Tour"
         className="h-full w-full border-0"
         allow="fullscreen; xr-spatial-tracking; autoplay"
